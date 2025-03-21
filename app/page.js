2a3af1cd88cd55eb341790ba8f25742cb7edd4f5
itsvanshchavda <vanshchavda111@gmail.com>
2025-03-21 23:41:06 +0530

    Add dashboard page and update landing page with logo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
+import logo from '../public/logo.png';
 
 const page = () => {
   return (
-    <div className="h-60 flex justify-center items-center">
+    <div className="h-full flex justify-center items-center">
       <div>
+      <Image src={logo} alt="Logo" className="w-auto h-10" />
         <h1 className="text-xl font-medium">Overnight AI - Landing Page</h1>
 
         <Button>Let's Start</Button>
